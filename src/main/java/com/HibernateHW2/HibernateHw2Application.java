8a19405f2e0554cac0e1fd32a0cf3e9b27ad9adb
Oleg <olegrikov@ya.ru>
2021-06-01 19:41:38 +0300

    Initial commit

---
+++ src/main/java/com/HibernateHW2/HibernateHw2Application.java
@@ -0,0 +1,13 @@
+package com.HibernateHW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateHw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/HibernateHW2/HibernateHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HibernateHW2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
